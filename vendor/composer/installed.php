6c0f209b7d93b0a995e0b10aed5c466347480d48
natthamon0 <natthamon.k@merudy.com>
2026-03-20 09:59:52 +0700

    Add Composer autoload files and initial setup
    - Created autoload.php for Composer autoloading functionality.
    - Implemented ClassLoader.php to handle PSR-0 and PSR-4 autoloading.
    - Added InstalledVersions.php to manage installed package versions.
    - Included LICENSE file for Composer licensing information.
    - Generated autoload_classmap.php, autoload_namespaces.php, autoload_psr4.php, and autoload_static.php for class mapping and autoloading.
    - Created installed.json and installed.php to track installed packages and their versions.
    - Added platform_check.php to ensure PHP version compatibility for Composer dependencies.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'namronggroup/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b80b12b99c8b3b3cd2896c3754faa4791544a21f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'namronggroup/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b80b12b99c8b3b3cd2896c3754faa4791544a21f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
